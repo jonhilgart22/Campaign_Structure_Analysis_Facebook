fcf9787d3d54fd182b90ffe348598dde02705c33
jonhilgart22 <Jonathan.Hilgart@gmail.com>
2015-08-13 17:14:56 -0700

    Initial commit

---
+++ Campaign_Diagnostic.py
@@ -0,0 +1,13 @@
+__author__ = 'Jonathan'
+
+#initial imports
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from pandas import Series,DataFrame
+
+print "Hello World"
+
