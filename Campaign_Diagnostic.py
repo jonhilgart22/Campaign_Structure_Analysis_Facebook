5d69a56837648b2907a00b3b96faf686cfa69bc0
jonhilgart22 <Jonathan.Hilgart@gmail.com>
2015-08-14 16:58:17 -0700

    starting working on 1:1:1 structure

--- Campaign_Diagnostic.py
+++ Campaign_Diagnostic.py
@@ -14,39 +14,58 @@ import csv
 print "Hello World"
 
 
-#Intial commit to turn the JSON into a Dataframe
-#df = pd.read_json('api url to hit and get back data')
+#Turn csv into a JSON object
 
-sc_df = pd.read_csv('C:\Users\Jonathan\Downloads\KV Report, Account hothead_04, 2015-07-15 - 2015-08-14.csv')
+csv_sc_df = open('C:\Users\Jonathan\Downloads\KV Report, Account hothead_04, 2015-07-15 - 2015-08-14.csv')
+json_sc_df = open("C:\Users\Jonathan\Downloads\KV Report, Account hothead_04, 2015-07-15 - 2015-08-14.json","w+")
+
+
+fieldnames = ('\xef\xbb\xbf"Reporting Starts',"Reporting Ends","Campaign Name","Ad Set Name","Ad Name","Impressions","Amount Spent (USD)","Clicks","Gender","Age","Website Clicks [28 Days After Clicking]","Ad Set ID","Campaign ID","Mobile App Installs [28 Days After Clicking]","Ad ID")
+reader=csv.DictReader(csv_sc_df,fieldnames)
+
+#count the number of rows in the csv
+csv_count = 0
+for row in reader:
+    #print row
+    json.dump(row, json_sc_df)
+    json_sc_df.write('\n')
+    csv_count+=1
+print csv_count, "the number of rows convereted from the csv into JSON"
+
+json_data=(json_sc_df).read()
+json_sc = json.loads(json_data)
+print json_sc
+
+
 
 #Get a list of every column header
-column_names=[]
-for column in sc_df:
-    column_names.append(column)
-print column_names
+#column_names=[]
+#for column in sc_df:
+ #   column_names.append(column)
+#print column_names
 
 #Filtering by campaign ids
 
 #1:1:1
 #find out the unique campaign ids
-unique_campaign_ids = []
-all_campaign_ids = []
-count = 0
-total_count=0
-for row in sc_df['Campaign ID']:
-    total_count+=1
-    if row not in unique_campaign_ids:
-        unique_campaign_ids.append(row)
-        count +=1
-    else:
-        all_campaign_ids.append(row)
-
-print unique_campaign_ids
-print "count", count
-print "total campaign count",total_count
-
-#Next, match up individual ad set ids to these campaign ids
-
+# all_campaign_ids = []
+# count = 0
+# unique_campaign_ids = []
+# total_count=0
+# for row in sc_df['Campaign ID']:
+#     total_count+=1
+#     if row not in unique_campaign_ids:
+#         unique_campaign_ids.append(row)
+#         count +=1
+#     else:
+#         all_campaign_ids.append(row)
+#
+# print unique_campaign_ids
+# print "count", count
+# print "total campaign count",total_count
+#
+# #Next, match up individual ad set ids to these campaign ids
+#
 
 
 
